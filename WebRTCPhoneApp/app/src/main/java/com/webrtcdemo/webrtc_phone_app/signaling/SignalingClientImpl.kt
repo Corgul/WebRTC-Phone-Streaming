ab43aa6fd1f17d5934f4699cfa36e32a2ab106af
Corgul <murray.josh7@gmail.com>
2021-12-17 00:22:17 -0500

    Remove unnecessary code

--- WebRTCPhoneApp/app/src/main/java/com/webrtcdemo/webrtc_phone_app/signaling/SignalingClientImpl.kt
+++ WebRTCPhoneApp/app/src/main/java/com/webrtcdemo/webrtc_phone_app/signaling/SignalingClientImpl.kt
@@ -30,32 +30,7 @@ import io.socket.client.Manager
 @Singleton
 class SignalingClientImpl @Inject constructor() : SignalingClient {
     private val roomEvents = MutableStateFlow<SocketRoomEvent?>(null)
-    private val socket: Socket
-
-    // Should not be in production
-    private val trustAllCerts = arrayOf<TrustManager>(object : X509TrustManager {
-        @SuppressLint("TrustAllX509TrustManager")
-        override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-        }
-
-        @SuppressLint("TrustAllX509TrustManager")
-        override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
-        }
-
-        override fun getAcceptedIssuers(): Array<X509Certificate> {
-            return arrayOf()
-        }
-    })
-
-    init {
-        val sslContext = SSLContext.getInstance("TLS")
-        sslContext.init(null, trustAllCerts, null)
-//        IO.setDefaultHostnameVerifier() { _: String, _: SSLSession ->
-//            true
-//        }
-//        IO.setDefaultSSLContext(sslContext)
-        socket = IO.socket("http://192.168.0.162:8080")
-    }
+    private val socket: Socket = IO.socket("http://192.168.0.162:8080")
 
     override fun connect(roomName: String) {
         roomEvents.value = SocketRoomEvent.CONNECTING
@@ -68,34 +43,23 @@ class SignalingClientImpl @Inject constructor() : SignalingClient {
     }
 
     private fun setupCallbacks() {
-        socket.on(Socket.EVENT_DISCONNECT) {
-            WebRTCAppLogger.d("Disconnected ${it.contentToString()}")
-        }
-        socket.on(Manager.EVENT_ERROR) {
-            WebRTCAppLogger.d("error")
-        }
         // log event
         socket.on("log") { args: Array<Any> ->
             WebRTCAppLogger.d("log call() called with: args = [" + args.contentToString() + "]")
         }
         socket.on("created") {
-            WebRTCAppLogger.d("onRoomEvent2 created, ${it.contentToString()}")
             roomEvents.value = SocketRoomEvent.CREATED_ROOM
         }
         socket.on("joined") {
-            WebRTCAppLogger.d("onRoomEvent2 joined, ${it.contentToString()}")
             roomEvents.value = SocketRoomEvent.JOINED_EXISTING_ROOM
         }
         socket.on("join") {
-            WebRTCAppLogger.d("onRoomEvent2 join, ${it.contentToString()}")
             roomEvents.value = SocketRoomEvent.PEER_JOINED_ROOM
         }
         socket.on("full") {
-            WebRTCAppLogger.d("onRoomEvent2 full, ${it.contentToString()}")
             roomEvents.value = SocketRoomEvent.ROOM_IS_FULL
         }
         socket.on("ready") {
-            WebRTCAppLogger.d("onRoomEvent2 ready, ${it.contentToString()}")
         }
     }
 
